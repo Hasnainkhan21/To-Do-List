2306902df9b41970101a0558b22b9aa00ad31aa9
naino <Hasnainkhan21>
2024-09-26 00:35:07 +0500

    js completed

--- index.js
+++ index.js
@@ -2,13 +2,31 @@ function getOutput() {
     var x = document.getElementById("inputText");
     var y = document.getElementById("taskList");
 
-    //handeling empty
-    if(!x.value.trim()){
-        return alert("todo can't be empty");
+    if (!x.value.trim()) {
+        return alert("Todo can't be empty");
     }
 
     var li = document.createElement("li");
-    li.textContent = inputText.value;
+    li.style.display = 'block';
+    li.textContent = x.value;
+    y.style.display = "block";
 
+    var remove = document.createElement('button');
+    remove.textContent = 'remove';
+    remove.className = 'remove-btn';
+    remove.onclick = function () {
+        y.removeChild(li);
+        // y.style.display = 'none';
+    };
 
+    li.onclick = function () {
+        li.classList.toggle('completed');
+    };
+
+    li.appendChild(remove);
+    y.appendChild(li);
+
+    x.value = '';
 }
+
+
