3ba861c6bc1405d449650ce12f4d790ddfb16050
naino <Hasnainkhan21>
2024-09-25 23:17:51 +0500

    js values called

--- index.js
+++ index.js
@@ -2,5 +2,13 @@ function getOutput() {
     var x = document.getElementById("inputText");
     var y = document.getElementById("taskList");
 
-    
+    //handeling empty
+    if(!x.value.trim()){
+        return alert("todo can't be empty");
+    }
+
+    var li = document.createElement("li");
+    li.textContent = inputText.value;
+
+
 }
